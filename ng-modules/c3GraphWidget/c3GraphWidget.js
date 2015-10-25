2b0bb8a4cccff757ee4712362d002933ea0e3176
Tylar Murray <murray.tylar@gmail.com>
2015-10-24 22:05:44 -0400

    sample graph in place

---
+++ ng-modules/c3GraphWidget/c3GraphWidget.js
@@ -0,0 +1,19 @@
+/**
+ * graph widget for showing customizable time-series
+ * Created by tylar on 2015-10-24
+ */
+require("c3-angularjs");
+
+angular.module('c3GraphWidget', [
+    "c3-angularjs"
+])
+    .controller('c3GraphWidgetController', ['$scope', function($scope) {
+    }])
+   .directive("c3GraphWidget", function() {
+    return {
+        restrict: 'E',
+        templateUrl: "ng-modules/c3GraphWidget/c3GraphWidget.html"
+    };
+});
+
+module.exports = angular.module('c3GraphWidget').name;
