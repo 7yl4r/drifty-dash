7yl4r/drifty-dash

commit 813569d44e112c1eaa068145f776118f8e967afa

minor synatical & doc changes

ng-modules/c3GraphWidget/c3GraphWidget.js
@@ -9,33 +9,39 @@ angular.module('c3GraphWidget', [
     require("ui.bootstrap")
 ])
     .controller('c3GraphWidgetController', ['$scope', function($scope) {
-         $scope.series =[
-             {
-                 "instrument": "neutron spectrometer 1",
-                 "collimation": 80,
-                 "collimation_units" : "minutes",
-                 "aperture_w" : 0.5,
-                 "aperture_h" : 0.5,
-                 "aperture_units" : "inches",
-                 "energy": [13.5, 30.5, 41, 60],
-                 "energyUnits": "meV",
-                 "flux": [1.69e7, 3.97e7, 5.17e7, 4.11e7],
-                 "fluxUnits": "n/(cm**2 * s)"
-             },{
-                 "instrument": "neutron spectrometer 3",
-                 "collimation": 80,
-                 "collimation_units" : "minutes ",
-                 "aperture_w" : 0.5,
-                 "aperture_h" : 0.5,
-                 "aperture_units" : "inches",
-                 "energy": [14.7, 30.5, 41, 60],
-                 "energyUnits": "meV",
-                 "flux": [1.63e7, 4.60e7, 6.07e7, 6.78e7],
-                 "fluxUnits": "n/(cm**2 * s)"
-             }
-         ];
+        $scope.series =[
+            {
+                "instrument": "neutron spectrometer 1",
+                "collimation": 80,
+                "collimation_units" : "minutes",
+                "aperture_w" : 0.5,
+                "aperture_h" : 0.5,
+                "aperture_units" : "inches",
+                "energy": [13.5, 30.5, 41, 60],
+                "energyUnits": "meV",
+                "flux": [1.69e7, 3.97e7, 5.17e7, 4.11e7],
+                "fluxUnits": "n/(cm**2 * s)"
+            },{
+                "instrument": "neutron spectrometer 3",
+                "collimation": 80,
+                "collimation_units" : "minutes ",
+                "aperture_w" : 0.5,
+                "aperture_h" : 0.5,
+                "aperture_units" : "inches",
+                "energy": [14.7, 30.5, 41, 60],
+                "energyUnits": "meV",
+                "flux": [1.63e7, 4.60e7, 6.07e7, 6.78e7],
+                "fluxUnits": "n/(cm**2 * s)"
+            }
+        ];
 
-        var formatData = function() {
+        $scope.updateSeries = function(instrumentName){
+
+        };
+
+        var updateData = function() {
+            // updates graph data using $scope.series
+            // converts to proper data format for c3
             var XS = {};
             var cols = [];
             for (var i = 0; i < $scope.series.length; i++) {
@@ -74,7 +80,7 @@ angular.module('c3GraphWidget', [
                 }
             };
         };
-        formatData();  // init format data
+        updateData();  // init format data
 
         $scope.addItem = function() {
             var newItemNo = $scope.series.length + 1;

commit 3851167d55fb8c06f3c336443c69e7f8cc24add9

fix no-data-update bug introduced in 813569d44e112c1eaa068145f776118f8e967afa

ng-modules/c3GraphWidget/c3GraphWidget.js
@@ -58,14 +58,14 @@ angular.module('c3GraphWidget', [
             console.log("update series #" + index);
             var text = document.getElementById("series-json-"+index).value;
             $scope.series[index] = JSON.parse(text);
-            formatData();
+            updateData();
         };
 
         $scope.removeSeries = function(index){
             console.log("del series #" + index);
             delete $scope.series[index];  // frees memory?
             $scope.series = $scope.series.slice(0,index).concat($scope.series.slice(index + 1));  // actual remove from array
-            formatData();
+            updateData();
         };
 
         $scope.addSeries = function(){