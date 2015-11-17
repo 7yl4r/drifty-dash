64cc59b65d53e840cecd03bfa5d416f6aa357826
Tylar Murray <murray.tylar@gmail.com>
2015-11-17 00:32:34 -0500

    added createSeries() for default values. DRY++

--- ng-modules/c3GraphWidget/c3GraphWidget.js
+++ ng-modules/c3GraphWidget/c3GraphWidget.js
@@ -10,35 +10,49 @@ angular.module('c3GraphWidget', [
     require("ui.bootstrap")
 ])
     .controller('c3GraphWidgetController', ['$scope', function($scope) {
+        var DEF_FLUX_UNITS   =      "n/(cm**2 * s)";
+        var DEF_ENERGY_UNITS =      "meV";
+        var DEF_COLLIMATION_UNITS = "minutes";
+        var DEF_APERTURE_UNITS =    "inches";
+
+        $scope.createSeries = function(seriesObj){
+            // adds default series values to given object if not already specified.
+            var def_values = {
+                "energy": [],
+                "flux": [],
+                "collimation_units": DEF_COLLIMATION_UNITS,
+                "aperture_units": DEF_APERTURE_UNITS,
+                "energyUnits": DEF_ENERGY_UNITS,
+                "fluxUnits": DEF_FLUX_UNITS
+            };
+
+            for(var key in def_values) {
+                if (typeof seriesObj[key] === 'undefined') {
+                    seriesObj[key] = def_values[key];
+                } // else do not overwrite values already in seriesObj
+            }
+
+            return seriesObj;
+        };
+
         $scope.series =[
-            {
+            $scope.createSeries({
                 "instrument": "neutron spectrometer 1",
                 "collimation": 80,
-                "collimation_units" : "minutes",
                 "aperture_w" : 0.5,
                 "aperture_h" : 0.5,
-                "aperture_units" : "inches",
                 "energy": [13.5, 30.5, 41, 60],
-                "energyUnits": "meV",
                 "flux": [1.69e7, 3.97e7, 5.17e7, 4.11e7],
-                "fluxUnits": "n/(cm**2 * s)"
-            },{
+            }),
+            $scope.createSeries({
                 "instrument": "neutron spectrometer 3",
                 "collimation": 80,
-                "collimation_units" : "minutes ",
                 "aperture_w" : 0.5,
                 "aperture_h" : 0.5,
-                "aperture_units" : "inches",
                 "energy": [14.7, 30.5, 41, 60],
-                "energyUnits": "meV",
                 "flux": [1.63e7, 4.60e7, 6.07e7, 6.78e7],
-                "fluxUnits": "n/(cm**2 * s)"
-            }
+            })
         ];
-
-        $scope.updateSeries = function(instrumentName){
-
-        };
 
         $scope.updateSeries = function(index){
             console.log("update series #" + index);
@@ -56,12 +70,10 @@ angular.module('c3GraphWidget', [
 
         $scope.addSeries = function(){
             console.log("adding new series");
-            $scope.series.push({
-                 "instrument": "inst " + $scope.series.length,
-                  "energy": [],
-                  "flux": [],
-            });
-            formatData();
+            $scope.series.push($scope.createSeries({
+                 "instrument": "inst " + $scope.series.length
+            }));
+            updateData();
         };
 
         var updateData = function() {
